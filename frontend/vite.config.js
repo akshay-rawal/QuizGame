3804bea74fca6fbafb1afd9e894defb945b012fb
Akshay Rawal <rawalakshay99@gmail.com>
2024-12-31 08:54:47 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     proxy: {
       '/api': {
-        target: 'https://quizgame-8.onrender.com',  // Your backend URL
+        target: 'http://localhost:4001',  // Your backend URL
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')  // Optional rewrite if necessary
